22bdada6ea06ae30ecb95ebcca29b4df86e49b37
Sonny D <damicose14@gmail.com>
2019-08-15 14:08:19 -0500

    sandbox js with user input storage functionality. Awaiting element IDs and queryURL variable

---
+++ sandbox.js
@@ -0,0 +1,16 @@
+// Given input1(Start) input2(End) input 3(Artist) input4(Track)
+
+// Given submit button(submitBtn)
+
+// Take text from each input upon button click
+// Store each in a variable
+
+startLocation = document.getElementById(input1).value
+endLocation = document.getElementById(input2).value
+artistName = document.getElementById(input3).value
+trackName = document.getElementById(input4).value
+
+// If re-formatting of input is necessary to be concatenated--do it below
+
+//Take variable value and concatenate into respective, provided queryURLs
+
