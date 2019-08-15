5488c0b6c44f0dcdf037cf9c630e60e15e0c195f
Jackson Oppenheim <jacksonopp@gmail.com>
2019-08-15 18:41:01 -0500

    put DOM code at top of program

--- assets/app.js
+++ assets/app.js
@@ -4,6 +4,25 @@ https://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key=c7c92f78a10b96b8
 
 */
 
+// Given input1(Start) input2(End) input 3(Track) input4(Artist)
+
+// Given submit button(submitBtn)
+
+// Take text from each input upon button click
+// Store each in a variable
+
+const startLocation = document.getElementById("starting-location").value;
+const endLocation = document.getElementById("ending-location").value;
+const trackName = document.getElementById("song-title").value;
+const artistName = document.getElementById("artist-name").value;
+
+// If re-formatting of input is necessary to be concatenated--do it below
+// toLowerCase()    ???
+
+//Take variable value and concatenate into respective, provided queryURLs
+
+
+
 function getTrackLength(artist, track) {
 
     const apiKey = "c7c92f78a10b96b8086988432a4f4cf5"; // my api key for last.fm audioscrobbler
@@ -49,21 +68,4 @@ function getDirectionInfo(fromLocation, toLocation) {
 }
 
 getDirectionInfo("1890 Buford Ave, Saint Paul, MN", "11367 Fowlers Mill Rd, Chardon, OH 44024");
-
-// Given input1(Start) input2(End) input 3(Track) input4(Artist)
-
-// Given submit button(submitBtn)
-
-// Take text from each input upon button click
-// Store each in a variable
-
-const startLocation = document.getElementById(starting-location).value;
-const endLocation = document.getElementById(ending-location).value;
-const trackName = document.getElementById(song-title).value;
-const artistName = document.getElementById(artist-name).value;
-
-// If re-formatting of input is necessary to be concatenated--do it below
-// toLowerCase()    ???
-
-//Take variable value and concatenate into respective, provided queryURLs
 
