d0a6b54eca37ffb17d87dfa7dac975ef6e6928db
Jackson Oppenheim <jacksonopp@gmail.com>
2019-08-15 17:38:44 -0500

    added direction fetch

--- assets/app.js
+++ assets/app.js
@@ -22,3 +22,17 @@ function getTrackLength(artist, track) {
 }
 
 getTrackLength("cher", "believe");
+
+function getDirectionInfo(fromLocation, toLocation) {
+    const apiKey = "1ar8EgSpyQGUCgm8HV9dyZhG7AWbPq7a"
+
+    const queryURL = "http://open.mapquestapi.com/directions/v2/route?key=" + apiKey + "&from=" + fromLocation + "&to=" + toLocation + "&unit=m";
+
+    fetch(queryURL).then(function (response) {
+        return response.json();
+    }).then(function (responseJson) {
+        console.log(responseJson);
+    })
+}
+
+getDirectionInfo("1605 Minnehaha Ave W, Saint Paul, MN, 55104", "11367 Fowlers Mill Rd, Chardon, OH 44024");
