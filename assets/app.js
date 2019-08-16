c6894615fa3f9d5b58d4b272522f7c76b9b6c735
Jackson Oppenheim <jacksonopp@gmail.com>
2019-08-16 10:26:30 -0500

    fetches happn in this order: direction then music

--- assets/app.js
+++ assets/app.js
@@ -27,52 +27,42 @@ submitButton.addEventListener("click", function () {
     const artistName = document.getElementById("artist-name").value;
 
 
-
+    //sets up the function to get track length
     function getTrackLength(artist, track) {
 
         const apiKey = "c7c92f78a10b96b8086988432a4f4cf5"; // my api key for last.fm audioscrobbler
 
         const queryURL = "https://ws.audioscrobbler.com/2.0/?method=track.getInfo" + "&api_key=" + apiKey + "&artist=" + artist + "&track=" + track + "&format=json"; // queryURL to be used in fetch 
-
-
-
-
         fetch(queryURL).then(function (response) {
             return response.json()
         }).then(function (responseJson) {
             console.log(responseJson);  // console log json to check integrity
-
-            const songLength = parseInt(responseJson.track.duration) / 1000;
+            const songLength = responseJson.track.duration; //this returns the song length
             console.log("song length:", songLength);
         })
 
     }
 
-    getTrackLength(artistName, trackName);
 
     function getDirectionInfo(fromLocation, toLocation) {
         const apiKey = "1ar8EgSpyQGUCgm8HV9dyZhG7AWbPq7a"
-
         const queryURL = "http://www.mapquestapi.com/directions/v2/route?key=" + apiKey + "&from=" + fromLocation + "&to=" + toLocation + "&unit=m";
-
         console.log(queryURL);
-
         fetch(queryURL).then(function (response) {
             return response.json();
         }).then(function (responseJson) {
             console.log(responseJson);
-
             distanceInMiles = responseJson.route.distance;
             distanceInKm = distanceInMiles * 1.609344;
-            driveTime = responseJson.route.time; //in minutes
-            driveTimeHrs = driveTime / 60 / 60; //in hours
-
+            driveTime = responseJson.route.time; //returns drive time in minutes
             console.log("drive time in hours: ", driveTimeHrs);
             console.log("distance in miles: ", distanceInMiles);
             console.log("distance in km: ", distanceInKm);
+
+            getTrackLength(artistName, trackName); //runs the trackLength function
         })
     }
 
-    getDirectionInfo(startLocation, endLocation);
+    getDirectionInfo(startLocation, endLocation); //runs the get direction info
 
 })
