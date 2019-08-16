c9b71b2153560f5bf99ac9bb4399d1aff7651d5f
Jackson Oppenheim <jacksonopp@gmail.com>
2019-08-15 23:01:36 -0500

    fetches happen on submit click

--- assets/app.js
+++ assets/app.js
@@ -11,61 +11,68 @@ https://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key=c7c92f78a10b96b8
 // Take text from each input upon button click
 // Store each in a variable
 
-const startLocation = document.getElementById("starting-location").value;
-const endLocation = document.getElementById("ending-location").value;
-const trackName = document.getElementById("song-title").value;
-const artistName = document.getElementById("artist-name").value;
 
 // If re-formatting of input is necessary to be concatenated--do it below
 // toLowerCase()    ???
 
 //Take variable value and concatenate into respective, provided queryURLs
 
+const submitButton = document.getElementById("submit-btn")
+
+submitButton.addEventListener("click", function () {
+
+    const startLocation = document.getElementById("starting-location").value;
+    const endLocation = document.getElementById("ending-location").value;
+    const trackName = document.getElementById("song-title").value;
+    const artistName = document.getElementById("artist-name").value;
 
 
-function getTrackLength(artist, track) {
 
-    const apiKey = "c7c92f78a10b96b8086988432a4f4cf5"; // my api key for last.fm audioscrobbler
+    function getTrackLength(artist, track) {
 
-    const queryURL = "https://ws.audioscrobbler.com/2.0/?method=track.getInfo" + "&api_key=" + apiKey + "&artist=" + artist + "&track=" + track + "&format=json"; // queryURL to be used in fetch 
+        const apiKey = "c7c92f78a10b96b8086988432a4f4cf5"; // my api key for last.fm audioscrobbler
+
+        const queryURL = "https://ws.audioscrobbler.com/2.0/?method=track.getInfo" + "&api_key=" + apiKey + "&artist=" + artist + "&track=" + track + "&format=json"; // queryURL to be used in fetch 
 
 
 
 
-    fetch(queryURL).then(function (response) {
-        return response.json()
-    }).then(function (responseJson) {
-        console.log(responseJson);  // console log json to check integrity
+        fetch(queryURL).then(function (response) {
+            return response.json()
+        }).then(function (responseJson) {
+            console.log(responseJson);  // console log json to check integrity
 
-        const songLength = parseInt(responseJson.track.duration) / 1000;
-        console.log("song length:", songLength);
+            const songLength = parseInt(responseJson.track.duration) / 1000;
+            console.log("song length:", songLength);
+        })
+
+    }
+
+    getTrackLength(artistName, trackName);
+
+    function getDirectionInfo(fromLocation, toLocation) {
+        const apiKey = "1ar8EgSpyQGUCgm8HV9dyZhG7AWbPq7a"
+
+        const queryURL = "http://www.mapquestapi.com/directions/v2/route?key=" + apiKey + "&from=" + fromLocation + "&to=" + toLocation + "&unit=m";
+
+        console.log(queryURL);
+
+        fetch(queryURL).then(function (response) {
+            return response.json();
+        }).then(function (responseJson) {
+            console.log(responseJson);
+
+            distanceInMiles = responseJson.route.distance;
+            distanceInKm = distanceInMiles * 1.609344;
+            driveTime = responseJson.route.time; //in minutes
+            driveTimeHrs = driveTime / 60 / 60; //in hours
+
+            console.log("drive time in hours: ", driveTimeHrs);
+            console.log("distance in miles: ", distanceInMiles);
+            console.log("distance in km: ", distanceInKm);
+        })
+    }
+
+    getDirectionInfo(startLocation, endLocation);
+
 })
-
-}
-
-getTrackLength("pavement", "spit on a stranger");
-
-function getDirectionInfo(fromLocation, toLocation) {
-    const apiKey = "1ar8EgSpyQGUCgm8HV9dyZhG7AWbPq7a"
-
-    const queryURL = "http://www.mapquestapi.com/directions/v2/route?key=" + apiKey + "&from=" + fromLocation + "&to=" + toLocation + "&unit=m";
-
-    console.log(queryURL);
-
-    fetch(queryURL).then(function (response) {
-        return response.json();
-    }).then(function (responseJson) {
-        console.log(responseJson);
-
-        distanceInMiles = responseJson.route.distance;
-        distanceInKm = distanceInMiles * 1.609344;
-        driveTime = responseJson.route.time;
-
-        console.log("drive time: ", driveTime);
-        console.log("distance in miles: ", distanceInMiles);
-        console.log("distance in km: ", distanceInKm);
-    })
-}
-
-getDirectionInfo("1890 Buford Ave, Saint Paul, MN", "11367 Fowlers Mill Rd, Chardon, OH 44024");
-
