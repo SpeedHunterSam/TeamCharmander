6a4b68ff767fb55e745240ffbd92c44696cae0bf
Sonny D <damicose14@gmail.com>
2019-08-19 12:11:04 -0500

    keeping all OMDB java in sandbox until ready for implement/testing. Storing movie TrekNum in separate variable. Created variables for movie title and year.

--- OMDB_sandbox.js
+++ OMDB_sandbox.js
@@ -1,6 +1,13 @@
+const movieTitle = document.getElementById("movie-title").value
+
+// const movieYear = document.getElementById("movie-year").value
+
+// If we need movie specificity==> Year would probably be easier to input for user vs. director, releaseDay, etc.
+
 function getMovieLength(movieTitle) {
     const apiKey = "d2c81adc"
     const queryURL = "https://www.omdbapi.com/?apikey=" + apiKey + "&t=" + movieTitle;
+    // If adding year specificity append the following==>| + "&y=" + movieYear|
     console.log(queryURL);
     fetch(queryURL).then(function (response) {
         return response.json();
@@ -8,10 +15,10 @@ function getMovieLength(movieTitle) {
         console.log(responseJson);
         const convertedMovieLength = ((responseJson.runtime) * 60);
         // convert output (minutes) to seconds
-        trackTreckNum = Math.ceil(convertedDistanceTime / convertedMovieLength)
+        flicTrekNum = Math.ceil(convertedDistanceTime / convertedMovieLength);
         console.log("running time: ", convertedMovieLength);
-        getMovieLength(movieTitle); //still need to output somewhere!!
-        document.getElementById("output").innerHTML = fromCity + " is " + trackTreckNum + " " + responseJson.title + "'s away from " + toCity + "!";
+        // getMovieLength(movieTitle); //still need to output somewhere!! (but not here)
+        document.getElementById("output").innerHTML = fromCity + " is " + flicTrekNum + " " + responseJson.title + "'s away from " + toCity + "!";
     })
 }
 // Create HTML elements for movie input
