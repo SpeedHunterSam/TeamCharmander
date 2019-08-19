218ef40846817345311efd9b6375c2cc77a61842
Jackson Oppenheim <jacksonopp@gmail.com>
2019-08-19 13:49:34 -0500

    click on album to display tracks

--- sandboxing/album_sandbox.js
+++ sandboxing/album_sandbox.js
@@ -1,3 +1,38 @@
+function searchAlbums(artist) {
+    let apiKey = "c7c92f78a10b96b8086988432a4f4cf5";
+
+    let queryURL = "http://ws.audioscrobbler.com/2.0/?method=artist.gettopalbums&api_key=" + apiKey + "&artist=" + artist + "&format=json";
+
+    fetch(queryURL).then(function (response) {
+        return response.json();
+    }).then(function (responseJson) {
+        console.log(responseJson);
+
+        const albumArray = responseJson.topalbums.album;
+
+        const answerDiv = document.getElementById("answer");
+        answerDiv.innerHTML = "";
+        answerUL = document.createElement("Ul");
+        answerDiv.append(answerUL);
+
+        for (i = 0; i < albumArray.length; i++) {
+            if (albumArray[i].name !== "(null)") {
+                answerLI = document.createElement("li");
+                answerLI.innerText = albumArray[i].name;
+                answerLI.setAttribute("data-album", albumArray[i].name);
+                answerLI.setAttribute("data-artist", artist);
+                answerLI.classList.add("album")
+                answerUL.append(answerLI);
+                answerLI.addEventListener("click", function (event) {
+                    albumSearch = event.target.getAttribute("data-album");
+                    getTrackLength(artist, albumSearch);
+                })
+            }
+        }
+    })
+}
+
+
 function getTrackLength(artist, album) {
 
     let apiKey = "c7c92f78a10b96b8086988432a4f4cf5";
@@ -8,32 +43,31 @@ function getTrackLength(artist, album) {
         return response.json();
     }).then(function (responseJson) {
         console.log(responseJson);
-        let albumArray = responseJson.album.tracks.track;
+        let trackArray = responseJson.album.tracks.track;
 
         let trackTimes = [];
 
-        answerDiv = document.getElementById("answer")
-        for (i = 0; i < albumArray.length; i++) {
-            console.log(albumArray[i].name);
-            answerUL = document.createElement("Ul");
-            answerLI = document.createElement("li");
-            answerLI.innerText = albumArray[i].name;
-            answerUL.append(answerLI);
-            answerDiv.append(answerUL);
+        const answerDiv = document.getElementById("answer")
+        answerDiv.innerHTML = "";
+        answerUL = document.createElement("ul");
+        answerDiv.append(answerUL);
 
-            trackTimes.push(parseInt(albumArray[i].duration));
+
+        for (i = 0; i < trackArray.length; i++) {
+            console.log(trackArray[i].name);
+            answerLI = document.createElement("li");
+            answerLI.innerText = trackArray[i].name + " : " + trackArray[i].duration;
+            answerUL.append(answerLI);
+            trackTimes.push(parseInt(trackArray[i].duration));
         }
         console.log(trackTimes);
         console.log(trackTimes.reduce((a, b) => a + b, 0));
     })
 }
 
-
 document.getElementById("submit").addEventListener("click", function () {
     const artistInput = document.getElementById("artist").value;
-    const albumInput = document.getElementById("album").value;
     console.log(artistInput);
-    console.log(albumInput);
 
-    getTrackLength(artistInput, albumInput);
+    searchAlbums(artistInput);
 })
