13d3810a8d55a686a0db0bc7d3d3fc59d90fe7e2
Jackson Oppenheim <jacksonopp@gmail.com>
2019-08-19 15:08:55 -0500

    added next/prev btn

--- sandboxing/album_sandbox.js
+++ sandboxing/album_sandbox.js
@@ -16,22 +16,47 @@ function searchAlbums(artist) {
         answerUL = document.createElement("Ul");
         answerDiv.append(answerUL);
 
+        let indexNum = 0;
         //displays the list of albums
-        for (i = 0; i < albumArray.length; i++) {
-            if (albumArray[i].name !== "(null)") {
-                answerLI = document.createElement("li");
-                answerLI.innerText = albumArray[i].name;
-                answerLI.setAttribute("data-album", albumArray[i].name);
-                answerLI.setAttribute("data-artist", artist);
-                answerLI.classList.add("album")
-                answerUL.append(answerLI);
-                answerLI.addEventListener("click", function (event) {
-                    albumSearch = event.target.getAttribute("data-album");
-                    //runs the get track length function
-                    getTrackLength(artist, albumSearch);
-                })
+        function displayAlbums(index) {
+            j = index + 9
+            for (i = index; i < j; i++) {
+                if (albumArray[i].name !== "(null)") {
+                    answerLI = document.createElement("li");
+                    answerLI.innerText = albumArray[i].name;
+                    answerLI.setAttribute("data-album", albumArray[i].name);
+                    answerLI.setAttribute("data-artist", artist);
+                    answerLI.classList.add("album")
+                    answerUL.append(answerLI);
+                    answerLI.addEventListener("click", function (event) {
+                        albumSearch = event.target.getAttribute("data-album");
+                        //runs the get track length function
+                        getTrackLength(artist, albumSearch);
+                    })
+                }
             }
         }
+        displayAlbums(indexNum);
+        const nextBtn = document.createElement("button");
+        const prevBtn = document.createElement("button");
+        nextBtn.innerText = ">";
+        prevBtn.innerText = "<"
+        nextBtn.addEventListener("click", function () {
+            if (indexNum < 40) {
+                indexNum = indexNum + 10;
+                answerUL.innerHTML = "";
+                displayAlbums(indexNum);
+            }
+        })
+        prevBtn.addEventListener("click", function () {
+            if (indexNum > 0) {
+                indexNum = indexNum - 10;
+                answerUL.innerHTML = "";
+                displayAlbums(indexNum);
+            }
+        })
+        answerDiv.append(prevBtn);
+        answerDiv.append(nextBtn);
     })
 }
 
