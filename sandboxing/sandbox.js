73cef8d31285693306d369b6e07b0c4c08872f68
Sonny D <damicose14@gmail.com>
2019-08-28 10:38:25 -0500

    created components for selecting means of transportation. Just needs to move from sandbox to live version

--- sandboxing/sandbox.js
+++ sandboxing/sandbox.js
@@ -1,8 +1,22 @@
 // Regarding Choice of Transportation
 
 // Need to create input field in HTML for allowing user to select between types of transportation
-// Need to create a new variable travelMethod that will store user input for type of transportation they've chosen
-// In queryURL const for the mapquest api we'll need to append a parameter for specifying the route type:
 
-// &transportMode=|Values can be: AUTO, WALKING, or BICYCLE| + travelMethod
+<div class="input-field col s12">
+<select id="travel-method">
+  <option value="" disabled selected>Select Mode of Transport</option>
+  <option value="AUTO">Automobile</option>
+  <option value="WALKING">Walking</option>
+  <option value="BICYCLE">Bicycle 3</option>
+</select>
+<label>Materialize Select</label>
+</div>
+
+// Need to create a new variable travelMethod that will store user input for type of transportation they've chosen
+
+const travelMethod = document.getElementById("travel-method").value
+
+// In queryURL const for the mapquest api we'll need to append a parameter for specifying the route type:
+// Values can be: AUTO, WALKING, or BICYCLE
+"&transportMode=" + travelMethod
 
