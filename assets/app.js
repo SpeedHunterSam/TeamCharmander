9020e6ab6fddc8580ad6fb34b2f7d176a8a785d9
SNelson <snelson1337@gmail.com>
2019-08-28 12:27:05 -0500

    Added user input validation for directions

--- assets/app.js
+++ assets/app.js
@@ -4,7 +4,7 @@ const submitButtonSong = document.getElementById("submit-btn-song");
 const outputDisplayP = document.getElementById("output");
 let trackTreckNum; //track length @ global scope for easy reference 
 
-M.Tabs.init(document.querySelector('.tabs'))
+//M.Tabs.init(document.querySelector('.tabs'))
 //Allows materialize tabs to actually appear
 
 //click submit button on song tab
@@ -29,6 +29,15 @@ submitButtonSong.addEventListener("click", function () {
 
     function checkValues(artist, track) {
         if (artist.indexOf("#") !== -1 || track.indexOf("#") !== -1) {
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+
+    function checkDirections(fromState, fromCity, toState, toCity) {
+        if (fromState === "" || fromCity === "" || toState === "" || toCity === "") {
             return false;
         }
         else {
@@ -84,46 +93,51 @@ submitButtonSong.addEventListener("click", function () {
         const apiKey = "1ar8EgSpyQGUCgm8HV9dyZhG7AWbPq7a"
         const queryURL = "https://www.mapquestapi.com/directions/v2/route?key=" + apiKey + "&from=" + fromCity + ", " + fromState + "&to=" + toCity + ", " + toState + "&unit=m";
         console.log(queryURL);
-        fetch(queryURL).then(function (response) {
-            return response.json();
-        }).then(function (responseJson) {
-            if (!responseJson.route.distance || responseJson.route.locations[0].adminArea3 !== fromState || responseJson.route.locations[1].adminArea3 !== toState || responseJson.route.locations[0].adminArea5 === "" || responseJson.route.locations[1].adminArea5 === "") {
-                console.log("Stop breaking our crap John.");
-            }
-            else {
-                console.log(responseJson);
+        if (checkDirections(fromState, fromCity, toState, toCity)) {
+            fetch(queryURL).then(function (response) {
+                return response.json();
+            }).then(function (responseJson) {
+                if (!responseJson.route.distance || responseJson.route.locations[0].adminArea3 !== fromState || responseJson.route.locations[1].adminArea3 !== toState || responseJson.route.locations[0].adminArea5 === "" || responseJson.route.locations[1].adminArea5 === "") {
+                    console.log("Stop breaking our crap John.");
+                }
+                else {
+                    console.log(responseJson);
+                    distanceInMiles = responseJson.route.distance;
+                    distanceInKm = distanceInMiles * 1.609344;
+                    driveTime = responseJson.route.time; //returns drive time in minutes
+                    driveTimeMin = driveTime / 60; //converting drive time to minutes from seconds
+
+                    console.log("drive time: ", driveTime);
+                    console.log("distance in miles: ", distanceInMiles);
+                    console.log("distance in km: ", distanceInKm);
+                    getTrackLength(artistName, trackName, driveTime, fromCity, toCity); //runs the trackLength function
+                    const driveAndTime = document.getElementById("driveAndTime")
+                    driveAndTime.classList.add("col", "s12");
+                    driveAndTime.innerHTML = "<br/>Drive Time in Minutes: " + driveTimeMin + "</br>Distance in Miles: " + distanceInMiles + "<br/> Distance in km: " + distanceInKm;
+                }
+                // getMovieLength(movieTitle)
+                console.log(responeJson);
                 distanceInMiles = responseJson.route.distance;
                 distanceInKm = distanceInMiles * 1.609344;
-                driveTime = responseJson.route.time; //returns drive time in minutes
+                driveTime = responseJson.route.time; //returns drive time in Seconds
                 driveTimeMin = driveTime / 60; //converting drive time to minutes from seconds
 
-                console.log("drive time: ", driveTime);
+                console.log("drive time in minutes: ", driveTime);
                 console.log("distance in miles: ", distanceInMiles);
                 console.log("distance in km: ", distanceInKm);
+
                 getTrackLength(artistName, trackName, driveTime, fromCity, toCity); //runs the trackLength function
-                const driveAndTime = document.getElementById("driveAndTime")
-                driveAndTime.classList.add("col", "s12");
-                driveAndTime.innerHTML = "<br/>Drive Time in Minutes: " + driveTimeMin + "</br>Distance in Miles: " + distanceInMiles + "<br/> Distance in km: " + distanceInKm;
-            }
-            // getMovieLength(movieTitle)
-            console.log(responseJson);
-            distanceInMiles = responseJson.route.distance;
-            distanceInKm = distanceInMiles * 1.609344;
-            driveTime = responseJson.route.time; //returns drive time in Seconds
-            driveTimeMin = driveTime / 60; //converting drive time to minutes from seconds
-
-            console.log("drive time in minutes: ", driveTime);
-            console.log("distance in miles: ", distanceInMiles);
-            console.log("distance in km: ", distanceInKm);
-
-            getTrackLength(artistName, trackName, driveTime, fromCity, toCity); //runs the trackLength function
 
 
-            // Adding the new paragraph to the viewport in HTML
-            document.getElementById("driveAndTime").innerHTML = "<br/>Drive Time in Minutes: " + driveTimeMin + "</br>Distance in Miles: " + distanceInMiles + "<br/> Distance in km: " + distanceInKm;
+                // Adding the new paragraph to the viewport in HTML
+                document.getElementById("driveAndTime").innerHTML = "<br/>Drive Time in Minutes: " + driveTimeMin + "</br>Distance in Miles: " + distanceInMiles + "<br/> Distance in km: " + distanceInKm;
 
 
-        })
+            })
+        }
+        else{
+            console.log("Stop breaking our crap John.");
+        }
     }
     getDirectionInfo(startState, startCity, endState, endCity); //runs the get direction info
 })
@@ -137,70 +151,76 @@ function searchAlbums(artist) {
     fetch(queryURL).then(function (response) {
         return response.json();
     }).then(function (responseJson) {
-        console.log(responseJson);
+        if (responseJson.error) {
+            console.log("Stop breaking our crap John.");
+            console.log(responseJson);
+        }
+        else {
+            console.log(responseJson);
 
-        const albumArray = responseJson.topalbums.album;
+            const albumArray = responseJson.topalbums.album;
 
-        const answerDiv = document.getElementById("answer");
-        answerDiv.innerHTML = "";
-        answerUL = document.createElement("ul");
-        answerArea = document.createElement("div");
-        answerDiv.append(answerUL);
-        answerDiv.append(answerArea);
+            const answerDiv = document.getElementById("answer");
+            answerDiv.innerHTML = "";
+            answerUL = document.createElement("ul");
+            answerArea = document.createElement("div");
+            answerDiv.append(answerUL);
+            answerDiv.append(answerArea);
 
-        //sets up function to displays the list of albums
-        let indexNum = 0;
-        function displayAlbums(index) {
-            //displays the i + 4 albums
-            j = index + 4
-            for (i = index; i < j; i++) {
-                if (albumArray[i].name !== "(null)") {
-                    // answerLI = document.createElement("li");
-                    answerImg = document.createElement("img");
+            //sets up function to displays the list of albums
+            let indexNum = 0;
+            function displayAlbums(index) {
+                //displays the i + 4 albums
+                j = index + 4
+                for (i = index; i < j; i++) {
+                    if (albumArray[i].name !== "(null)") {
+                        // answerLI = document.createElement("li");
+                        answerImg = document.createElement("img");
 
-                    answerImg.setAttribute("src", albumArray[i].image[2]["#text"]);
-                    answerImg.setAttribute("data-album", albumArray[i].name);
-                    answerImg.setAttribute("data-artist", artist);
-                    answerImg.classList.add("col", "s6");
-                    answerArea.append(answerImg);
+                        answerImg.setAttribute("src", albumArray[i].image[2]["#text"]);
+                        answerImg.setAttribute("data-album", albumArray[i].name);
+                        answerImg.setAttribute("data-artist", artist);
+                        answerImg.classList.add("col", "s6");
+                        answerArea.append(answerImg);
 
-                    // answerLI.innerText = albumArray[i].name;
-                    // answerLI.setAttribute("data-album", albumArray[i].name);
-                    // answerLI.setAttribute("data-artist", artist);
-                    // answerLI.classList.add("album")
-                    // answerUL.append(answerLI);
-                    answerImg.addEventListener("click", function (event) {
-                        albumSearch = event.target.getAttribute("data-album");
-                        //runs the get track length function
-                        getTrackLength(artist, albumSearch);
-                    })
+                        // answerLI.innerText = albumArray[i].name;
+                        // answerLI.setAttribute("data-album", albumArray[i].name);
+                        // answerLI.setAttribute("data-artist", artist);
+                        // answerLI.classList.add("album")
+                        // answerUL.append(answerLI);
+                        answerImg.addEventListener("click", function (event) {
+                            albumSearch = event.target.getAttribute("data-album");
+                            //runs the get track length function
+                            getTrackLength(artist, albumSearch);
+                        })
+                    }
                 }
             }
+            displayAlbums(indexNum);
+            const nextBtn = document.createElement("button");
+            const prevBtn = document.createElement("button");
+            const btnArea = document.createElement("div");
+            btnArea.classList.add("col", "s12");
+            nextBtn.innerText = ">";
+            prevBtn.innerText = "<";
+            nextBtn.addEventListener("click", function () {
+                if (indexNum < 40) {
+                    indexNum = indexNum + 5;
+                    answerArea.innerHTML = "";
+                    displayAlbums(indexNum);
+                }
+            })
+            prevBtn.addEventListener("click", function () {
+                if (indexNum > 0) {
+                    indexNum = indexNum - 5;
+                    answerArea.innerHTML = "";
+                    displayAlbums(indexNum);
+                }
+            })
+            answerDiv.append(btnArea);
+            btnArea.append(prevBtn)
+            btnArea.append(nextBtn);
         }
-        displayAlbums(indexNum);
-        const nextBtn = document.createElement("button");
-        const prevBtn = document.createElement("button");
-        const btnArea = document.createElement("div");
-        btnArea.classList.add("col", "s12");
-        nextBtn.innerText = ">";
-        prevBtn.innerText = "<";
-        nextBtn.addEventListener("click", function () {
-            if (indexNum < 40) {
-                indexNum = indexNum + 5;
-                answerArea.innerHTML = "";
-                displayAlbums(indexNum);
-            }
-        })
-        prevBtn.addEventListener("click", function () {
-            if (indexNum > 0) {
-                indexNum = indexNum - 5;
-                answerArea.innerHTML = "";
-                displayAlbums(indexNum);
-            }
-        })
-        answerDiv.append(btnArea);
-        btnArea.append(prevBtn)
-        btnArea.append(nextBtn);
     })
 }
 
