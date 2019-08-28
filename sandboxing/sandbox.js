b88247dd92ae3ad859b35f4c7e04922b2809b5ca
Sonny D <damicose14@gmail.com>
2019-08-27 22:23:34 -0500

    pseudo code for method of transportation input

---
+++ sandboxing/sandbox.js
@@ -0,0 +1,8 @@
+// Regarding Choice of Transportation
+
+// Need to create input field in HTML for allowing user to select between types of transportation
+// Need to create a new variable travelMethod that will store user input for type of transportation they've chosen
+// In queryURL const for the mapquest api we'll need to append a parameter for specifying the route type:
+
+// &transportMode=|Values can be: AUTO, WALKING, or BICYCLE| + travelMethod
+
